c850f7b02f785c847e5059d715441b0ec2bdc81b
Noel <noel@mean-machine-2.0>
2017-08-11 14:59:15 -0700

    First working version of code. Matthew says profiles don't make much sense, and Olavo proposed a different method to compute Structure functions

--- AQHR_StructureFunction.m
+++ AQHR_StructureFunction.m
@@ -1,4 +1,26 @@
-clc
-clear all
+function [D] = AQHR_StructureFunction(vstar, binheight)
 
-%Master code to compute the structure functions from a set of ADCP observations
+%Vstar is a matrix of along-beam velocity measurements where the vertical 
+%dimension is time and the horizontal is bins
+%
+%Dprofs is the matrix where structure function results are going to be stored.
+%
+%binheight is used to create the values of r^(2/3) when fitting D 
+%
+%D, fit and stats are the structure function for the set in question, the 
+%values of A and N (see Wiles et al. 2006) and the goodness-of-fit
+%statistics respectively.
+
+%Time average of differences in fluctuations u' between the first and all
+%other bins:
+
+vstar = vstar - nanmean(vstar); 
+
+D = nanmean((vstar(:,1) - vstar(:,4:end)).^2);
+
+%We now fit 
+
+end
+
+
+
