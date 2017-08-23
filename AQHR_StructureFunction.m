3cea591faabcec567a7dc40afcf07bceda48bb67
Noel Brizuela <nogutier@ucsd.edu>
2017-08-23 10:39:18 -0700

    Update AQHR_StructureFunction.m

--- AQHR_StructureFunction.m
+++ AQHR_StructureFunction.m
@@ -1,26 +1,44 @@
-function [D] = AQHR_StructureFunction(vstar, binheight)
+function [D] = AQHR_StructureFunction(vstar, r_min, r_max)
 
 %Vstar is a matrix of along-beam velocity measurements where the vertical 
-%dimension is time and the horizontal is bins
+%dimension is time and the horizontal is bins.
 %
-%Dprofs is the matrix where structure function results are going to be stored.
+%r_min and r_max must be given as separation between bins for which
+%structure functions will be computed and average. 
 %
-%binheight is used to create the values of r^(2/3) when fitting D 
+%Note that setting a large r_max may cause results to diverge from the r^(2/3) 
+%trend, since there will be few velocity differences to average for large r.
 %
-%D, fit and stats are the structure function for the set in question, the 
-%values of A and N (see Wiles et al. 2006) and the goodness-of-fit
-%statistics respectively.
+%vstar = detrend(vstar')';
+vstar = vstar - nanmean(vstar, 1);
+%According to Wiles et al (2006) we shouldn't compare consecutive bins, so 
+%r_min is usually set to 2.
+r_max = floor(r_max); r_min = floor(r_min); 
 
-%Time average of differences in fluctuations u' between the first and all
-%other bins:
 
-vstar = vstar - nanmean(vstar); 
+%Matrix form of the structure function, before temporal averaging
+Dmat = zeros(size(vstar,1),r_max - r_min + 1);
 
-D = nanmean((vstar(:,1) - vstar(:,4:end)).^2);
 
-%We now fit 
-
+%First we want to identify which values of r (distances between bins) we
+%can test and then how to find the relevant observations in vstar to make
+%those comparisons.
+for r = r_min:r_max
+    
+    %How many times can this r be measured inside our profile??
+    cases = size(vstar,2) - r;
+    %Entries Dmat(time,r) will be the average of the squared differences of
+    %along-beam velocity at separations r at time t.
+    
+    %Dmat(:,r-r_min+1) = mean((vstar(:,1:cases) - vstar(:,r+1:r+cases)).^2,2);
+    for k = 1:cases;
+        
+        %For all times (indicated by : in Dmat), we store the sum of
+        %squared differences between bins separated by r
+        Dmat(:,r-r_min+1) = Dmat(:,r-r_min+1) + (vstar(:,k) - vstar(:,k+r)).^2; 
+    end
+    Dmat(:,r-r_min+1) = Dmat(:,r-r_min+1)/cases;
 end
 
-
-
+D = nanmean(Dmat,1); %We average over time.
+end
